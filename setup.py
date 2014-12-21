d07094b49d8e76b47a26b65af7e479885e8a03c3
tmr232 <tmr232@github>
2014-12-22 01:01:51 +0200

    Added manifest and setup.cfg.

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-from distutils.core import setup
+import os
+
+from setuptools import setup
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
 
 setup(
     name='pywrap',
     version='0.1.0',
-    packages=['pywrap'],
+    py_modules=['pywrap'],
     url='https://github.com/tmr232/pywrap',
     license='MIT',
     author='Tamir Bahar',
     author_email='',
-    description='Ctypes made easy.'
+    description='Ctypes made easy.',
+    long_description=(read('README.rst')),
 )
